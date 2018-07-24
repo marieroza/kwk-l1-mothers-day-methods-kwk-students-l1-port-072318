f7e54fdceee3e286704f64f95ec1ac010df9ec6f
marieroza <marieeroza@gmail.com>
2018-07-24 18:24:58 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -2,4 +2,4 @@ puts "Welcome to the Holiday Greeting Card Maker!"
 def input(info = "Mom", info2 = "your favorite child", info3 = "Mother's Day")
   puts "Happy #{info3}, #{info}! - From #{info2}"
 end
-input
+input(info = "Beyonce", info2 = "J-z", info3 = "Fourth of July")
